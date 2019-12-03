971db6e38c1d5d3f7fd5bcfd2b9d46349868b80f
Alexis Bounds <boundsalexis@ucla.edu>
2019-12-03 12:22:19 -0800

    completed password generation

--- script.js
+++ script.js
@@ -21,41 +21,32 @@ function generate() {
     password = "";
     if ((Number.isInteger(length) === true) && (length > 7) && (length < 129)) {
         specialChar = confirm("Would you like to include special characters?");
-            if (specialChar === true) {
-                password += special[Math.floor(Math.random()*10)];
-                numChar = confirm("Would you like to include numeric characters?");
-            }
-            else {
-                numChar = confirm("Would you like to include numeric characters?");
-            }
-            if (numChar === true) {
-                password += numbers[Math.floor(Math.random()*10)];
-                lowerCaseChar = confirm("Would you like to include lower case characters?");
-            }
-            else {
-                lowerCaseChar = confirm("Would you like to include lower case characters?");
-            }
-            if (lowerCaseChar === true) {
-                password += lowCase[Math.floor(Math.random()* lowCase.length +1)];
-                upperCaseChar = confirm("Would you like to include upper case characters?");
-            }
-            else {
-                upperCaseChar = confirm("Would you like to include upper case characters?");
-            }
-            if  (upperCaseChar === true) {
-                password += upCase[Math.floor(Math.random()* upCase.length +1)];
-                console.log(password);
-            }
-            else{
-
-            } 
+        numChar = confirm("Would you like to include numeric characters?");
+        lowerCaseChar = confirm("Would you like to include lower case characters?");
+        upperCaseChar = confirm("Would you like to include upper case characters?");
+        while (password.length < length) {
+        if (specialChar === true && password.length < length) {
+            password += special[Math.floor(Math.random()*10)];
         }
+        if (numChar === true && password.length < length) {
+            password += numbers[Math.floor(Math.random()*10)];
+        }
+        if (lowerCaseChar === true && password.length < length) {
+            password += lowCase[Math.floor(Math.random()* lowCase.length +1)];
+        }
+        if  (upperCaseChar === true && password.length < length) {
+            password += upCase[Math.floor(Math.random()* upCase.length +1)];
+        }
+        } 
+    }
         else {
     alert("You must enter a number between 8 and 128");
         }
+ console.log(password);
+ console.log(length);
 }
     // console.log(password);
     
     function copy(){
-    document.write("placeholder");
+    document.write(password);
 }
